b35810f9b91e27d3236fb29f0dc8a141b4af6e22
atask <atask@users.noreply.github.com>
2015-03-19 00:10:08 +0100

    test(devices): Completed tests.

--- test/devices.js
+++ test/devices.js
@@ -6,38 +6,30 @@ import devices from '../src/devices';
 
 
 describe('devices', () => {
-    it('should return a list of connected devices', (done) => {
-        execFixtures.devices.none('aaa', function tst() {
-            console.log('assa');
-        });
-
-        var sp = execFixtures.pull.success();
-        sp.stderr.on('data', data => console.log('PULL_STDERR:' + data));
-        sp.on('close', () => {
-            console.log('PULL_CLOSE');
-            done();
-        });
-        /*
+    it('should return a list of connected devices (2 devices)', () => {
         mockery.registerMock('child_process', { exec: execFixtures.devices.two });
-
         var result = devices();
         result.should.be.an.Array;
         result.should.have.length(2);
         result.should.containEql('D5F3D60B50D481C7F4EB6857BB59A6D');
         result.should.containEql('10ec4f3e');
-
         mockery.deregisterMock('child_process');
-        done();
-        */
+    });
+
+    it('should return a list of connected devices (1 device)', () => {
+        mockery.registerMock('child_process', { exec: execFixtures.devices.one });
+        var result = devices();
+        result.should.be.an.Array;
+        result.should.have.length(1);
+        result.should.containEql('D5F3D60B50D481C7F4EB6857BB59A6D');
+        mockery.deregisterMock('child_process');
     });
 
     it('should return an empty list if no devices attached', () => {
         mockery.registerMock('child_process', { exec: execFixtures.devicesNone });
-
         var result = devices();
         result.should.be.an.Array;
         result.should.be.empty;
-
         mockery.deregisterMock('child_process');
     });
 });
