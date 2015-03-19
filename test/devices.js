fa74f18e0a18e459e2506b013ddabdcaa573c841
atask <atask@users.noreply.github.com>
2015-03-19 01:39:27 +0100

    chore(devices): adapted tests with promises

--- test/devices.js
+++ test/devices.js
@@ -2,18 +2,20 @@ import sinon from 'sinon';
 import mockery from 'mockery';
 
 import execFixtures from './fixtures';
-import devices from '../src/devices';
+import { devices } from '../lib/devices';
 
 
 describe('devices', () => {
-    it('should return a list of connected devices (2 devices)', () => {
+    it('should return a list of connected devices (2 devices)', done => {
         mockery.registerMock('child_process', { exec: execFixtures.devices.two });
-        var result = devices();
-        result.should.be.an.Array;
-        result.should.have.length(2);
-        result.should.containEql('D5F3D60B50D481C7F4EB6857BB59A6D');
-        result.should.containEql('10ec4f3e');
-        mockery.deregisterMock('child_process');
+        devices().then(result => {
+            result.should.be.an.Array;
+            result.should.have.length(2);
+            result.should.containEql('D5F3D60B50D481C7F4EB6857BB59A6D');
+            result.should.containEql('10ec4f3e');
+            mockery.deregisterMock('child_process');
+            done();
+        });
     });
 
     it('should return a list of connected devices (1 device)', () => {
