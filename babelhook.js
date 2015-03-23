59b3ae52ab92df57e3338859962aaecb52133f18
atask <atask@users.noreply.github.com>
2015-03-23 01:19:46 +0100

    chore(*): remove src cruft.

--- babelhook.js
+++ babelhook.js
@@ -1,4 +1,3 @@
-// This file is required in mocha.opts
 // The only purpose of this file is to ensure
 // the babel transpiler is activated prior to any
 // test code, and using the same babel options
